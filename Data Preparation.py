925e3d05697f128e8cfc6271a7c8e6fe46262dca
DefaultMatty <defaultmatty21@gmail.com>
2023-04-18 21:30:19 +0100

    Update Data Preparation.py

--- Data Preparation.py
+++ Data Preparation.py
@@ -10,18 +10,42 @@ import numpy as np
 import os
 
 year = '2022'
-path = os.getcwd() +"\\results\\"+ year+".csv"
-matches= pd.read_csv("C:\\Users\\matth\\OneDrive\\Desktop\\Independant Research Project\\allgames.csv")
+path = os.getcwd() +"\\Results\\"+ year+".csv"
+matches= pd.read_csv(path)
 
+
+def printfull (dataframe):
+   with pd.option_context('display.max_rows', None,
+                          'display.max_columns', None,
+                          'display.precision', 3,
+                          ):
+        print(dataframe)
+
+##printfull(matches.head())
 
 
 
 matches['HomexG']=matches['xG']
 matches['AwayxG']=matches['xG.1']
-matches['HomeGoals']=matches['Score'].str.split('\\|').str[0].astype(int)
-matches['AwayGoals']=matches['Score'].str.split('\\|').str[-1].astype(int)
 
-print("Path at terminal when executing this file")
-print(os.getcwd() + "\n")
+matches=matches[['Wk','Day','Date','Time','Home','HomexG','Home Goals','Away','AwayxG','Away Goals']]
 
 
+for i in range(0,len(matches)):
+    if matches.at[i,'Home Goals'] > matches.at[i,'Away Goals']:
+        matches.at[i, 'HomeResult']='W'
+        matches.at[i, 'AwayResult']='L'
+        matches.at[i, 'HomePoints']=3
+        matches.at[i, 'AwayPoints']=0
+    elif matches.at[i,'Home Goals'] < matches.at[i,'Away Goals']:
+        matches.at[i, 'HomeResult']='L'
+        matches.at[i, 'AwayResult']='W'
+        matches.at[i, 'HomePoints']=0
+        matches.at[i, 'AwayPoints']=3
+    else:
+        matches.at[i, 'HomeResult']='D'
+        matches.at[i, 'AwayResult']='D'
+        matches.at[i, 'HomePoints']=1
+        matches.at[i, 'AwayPoints']=1
+        
+PlayedMatches=matches[np.invert(np.isnan(matches['Home Goals']))]
